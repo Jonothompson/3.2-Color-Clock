ddcc8d9284ad776e36b044ff8459f00813d5ffc0
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-05-19 17:42:53 -0400

    got time bar and everyting working. just need to attack the background.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-var $hours = document.querySelector(".clock .hours");
-var $minutes = document.querySelector(".clock .minutes");
-var $seconds = document.querySelector(".clock .seconds");
+window.setInterval(currentTime, 1000);
 
-function currentTime () {
-	var clock = new Date();
-	console.log(clock)
+var $clock = document.querySelector(".clock");
+
+function currentTime() {
+	var clockTime = new Date();
+	var hours = clockTime.getHours();
+	var minutes = clockTime.getMinutes();
+	var seconds = clockTime.getSeconds();
+	hours = (hours < 10 ? "0" : "") + hours;
+	minutes = (minutes < 10 ? "0" : "") + minutes;
+	seconds = (seconds < 10 ? "0" : "") + seconds;
+	
+	$clock.textContent = hours + ":" + minutes + ":" +seconds;
+	
+	var timeBar = (seconds / 60) * 100;
+
+	var timeBarPercent = String(timeBar) + "%";
+	
+	document.querySelector(".expandingHr").style.width = timeBarPercent;
 }
+
+currentTime();
