fd5da147ae641b9f9b2b8c1263c39e53433409c7
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-05-19 14:48:00 -0400

    updated main.scss to add complete styling for page. and updated to index.html to display stock time.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var $hours = document.querySelector(".clock .hours");
+var $minutes = document.querySelector(".clock .minutes");
+var $seconds = document.querySelector(".clock .seconds");
+
 function currentTime () {
 	var clock = new Date();
 	console.log(clock)
